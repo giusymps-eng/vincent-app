4ea596216334f823cfce006cfbecfa0867ca6cfe
Giusy <giusymps@gmail.com>
2026-07-21 16:29:14 +0200

    Nuovo sito statico con menu

--- src/components/NewOrderButton.tsx
+++
@@ -1,9 +0,0 @@
-export function NewOrderButton() {
-  return (
-    <div className="px-6 mt-6">
-      <button className="w-full py-3 rounded-lg bg-primary text-white font-semibold hover:bg-orange-500 transition">
-        + NUOVO ORDINE
-      </button>
-    </div>
-  );
-}
--- src/data/categories.ts
+++
@@ -1,7 +0,0 @@
-export const categories = [
-  { id: "pizze", name: "Pizze" },
-  { id: "panini", name: "Panini" },
-  { id: "stuzzicherie", name: "Stuzzicherie" },
-  { id: "bevande", name: "Bevande" },
-  { id: "birre", name: "Birre" },
-];
